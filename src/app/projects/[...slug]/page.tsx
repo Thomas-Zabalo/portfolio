3fd8f6005cbdd590e11621660fdf76532a01b496
Thomas ZABALO <thomas.zabalo@gmail.com>
2025-02-10 19:12:52 +0100

    fix(app): Type pageProps

--- src/app/projects/[...slug]/page.tsx
+++ src/app/projects/[...slug]/page.tsx
@@ -3,34 +3,44 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import "../../prose.css";
 import Link from 'next/link';
+import { PageProps } from "next";
 
 interface Frontmatter {
   title: string;
 }
 
-export default async function ProjectPage({ params }: { params: { slug: string } }) {
+export default async function ProjectPage({ params }: PageProps<{ slug: string }>) {
+  if (!params || !params.slug) {
+    return <p>Projet introuvable.</p>;
+  }
+
   const filePath = path.join(process.cwd(), 'src/content', `${params.slug}.mdx`);
-  const content = await fs.readFile(filePath, 'utf-8');
 
-  const { content: mdxContent } = await compileMDX<Frontmatter>({
-    source: content,
-    options: {
-      parseFrontmatter: true
-    },
-    components: {
-      // Vos composants personnalisés ici
-    }
-  });
+  try {
+    const content = await fs.readFile(filePath, 'utf-8');
 
-  return (
-    <div className='pt-20 prose md:px-40 px-5'>
-      <Link href={`/projects`} className='transition-all duration-700'>
-        <button className="text-[#e40038]  rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
-          Retour
-        </button>
+    const { content: mdxContent } = await compileMDX<Frontmatter>({
+      source: content,
+      options: {
+        parseFrontmatter: true
+      },
+      components: {
+        // Vos composants MDX personnalisés ici
+      }
+    });
 
-      </Link>
-      {mdxContent}
-    </div>
-  );
+    return (
+      <div className='pt-20 prose md:px-40 px-5'>
+        <Link href={`/projects`} className='transition-all duration-700'>
+          <button className="text-[#e40038] rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+            Retour
+          </button>
+        </Link>
+        {mdxContent}
+      </div>
+    );
+  } catch (error) {
+    console.error("Erreur lors du chargement du fichier MDX :", error);
+    return <p>Erreur : Impossible de charger ce projet.</p>;
+  }
 }
