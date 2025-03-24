0dc817935e272743708eb47f3232ebd4c4a41f8d
tzabalo <t.zabalo@cdg82.fr>
2025-03-24 15:13:08 +0100

    fix(scroll): remove scroolsmooth

--- src/components/SmoothScrollWrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useSmoothScroll } from "@/utils/useSmoothScroll";
-
-export default function SmoothScrollWrapper() {
-  useSmoothScroll();
-  return null; // Ce composant ne rend rien, il applique juste le scroll
-}
