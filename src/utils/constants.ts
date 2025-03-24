fda47789c3bee24f33176321a9858969b8094da8
tzabalo <t.zabalo@cdg82.fr>
2025-03-24 15:13:52 +0100

    feat(projects): add constants file (#3)

---
+++ src/utils/constants.ts
@@ -0,0 +1,137 @@
+export const TECH_CATEGORIES = {
+    frontend: [
+        {
+            name: "Astro",
+            url: "https://astro.build",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/astro/astro-original.svg",
+        },
+        {
+            name: "React",
+            url: "https://react.dev",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        },
+        {
+            name: "Vue",
+            url: "https://vuejs.org/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
+        },
+        {
+            name: "Next.js",
+            url: "https://nextjs.org",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+        },
+        {
+            name: "Nuxt",
+            url: "https://nuxt.com/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nuxtjs/nuxtjs-original.svg",
+        },
+        {
+            name: "TypeScript",
+            url: "https://www.typescriptlang.org/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+        },
+        {
+            name: "MDX",
+            url: "https://mdxjs.com/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/markdown/markdown-original.svg"
+        }
+    ],
+    backendcms: [
+        {
+            name: "Node.js",
+            url: "https://nodejs.org/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+        },
+        {
+            name: "PHP",
+            url: "https://www.php.net/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
+        },
+        {
+            name: "WordPress",
+            url: "https://wordpress.com/fr/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-original.svg",
+        },
+        {
+            name: "TYPO3",
+            url: "https://typo3.org/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typo3/typo3-original.svg",
+        },
+    ],
+    mobile:[
+        {
+            name: React Native,
+            url: "https://reactnative.dev/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        },
+        {
+            name: Kotlin,
+            url:"https://kotlinlang.org/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg"
+        }
+    ],
+    uiux: [
+        {
+            name: "Figma",
+            url: "https://www.figma.com/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+        },
+        {
+            name: "Adobe XD",
+            url: "https://www.adobe.com/products/xd.html",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/xd/xd-plain.svg",
+        },
+        {
+            name: "Tailwind",
+            url: "https://tailwindcss.com/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg",
+        },
+        {
+            name: "Bootstrap",
+            url: "https://getbootstrap.com/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+        },
+    ],
+    devops: [
+        {
+            name: "Docker",
+            url: "https://www.docker.com/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+        },
+        {
+            name: "GitHub",
+            url: "https://github.com/",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        },
+    ],
+    3d: [
+        {
+            name: "Blender",
+            url: "https://www.blender.org/",
+            svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        },
+        {
+            name:"Unity",
+            url:"https://unity.com/fr",
+            svg:"https://github.com/devicons/devicon/blob/master/icons/unity/unity-original.svg",
+        }
+    ]
+};
+
+export const PROJECTS = [
+  {
+    name: "Escape Lab",
+    description: "Un jeu d'évasion en 1ère personne dans un laboratoire secret.",
+    github: "https://github.com/user/escape-lab",
+    date: "2025-03-24",
+    image: "/images/escape-lab.png",
+  },
+  {
+    name: "Akitomo",
+    description: "Portfolio inspiré du Japon traditionnel et des CRT.",
+    github: "https://github.com/user/akitomo",
+    date: "2025-02-10",
+    image: "/images/akitomo.png",
+  },
+];
+
