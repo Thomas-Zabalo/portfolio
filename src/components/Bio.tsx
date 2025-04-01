df908a69448593a1996728a84f0707918dab4eac
Thomas ZABALO <thomas.zabalo@gmail.com>
2025-04-01 10:05:16 +0200

    fix(image): display image

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -6,7 +6,7 @@ function Bio() {
     return (
         <section className="mx-auto px-4 max-w-xl py-16 pt-0 sm:pt-8">
             <div className="flex max-sm:flex-col gap-4">
-                <Image src="./Moi.jpg" alt="" loading="eager" width="128" height="128" decoding="async" className="h-24 w-24 sm:h-32 sm:w-32 rounded shrink-0" />
+                <Image src="/Moi.jpg" alt="" loading="eager" width="128" height="128" decoding="async" className="h-24 w-24 sm:h-32 sm:w-32 rounded shrink-0" />
                 <div>
                     <div className="flex justify-between items-center">
                         <h1 className="text-2xl font-semibold text-text">Thomas Zabalo</h1>
